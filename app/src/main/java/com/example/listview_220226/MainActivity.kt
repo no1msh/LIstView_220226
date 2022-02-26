917c02be8903ff73217fa67d0c4da4b07363c2c1
TFQ_Moon <no1msh1217@gmail.com>
2022-02-26 16:09:52 +0900

    리스트 뷰에서 아이템 클릭될 때 이벤트 처리

--- app/src/main/java/com/example/listview_220226/MainActivity.kt
+++ app/src/main/java/com/example/listview_220226/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.listview_220226
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.listview_220226.adapters.StudentAdapter
+import com.example.listview_220226.datas.StudentData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // 학생 목록을 담을 그릇
+    val mStudentList = ArrayList<StudentData>()
+
+    lateinit var mAdapter : StudentAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add( StudentData("조경진", 1988, "서울시 동대문구"))
+        mStudentList.add( StudentData("권효영", 1995, "서울시 중랑구") )
+        mStudentList.add( StudentData("김정환", 1981, "서울시 마포구") )
+        mStudentList.add( StudentData("김한균", 1977, "서울시 강남구") )
+        mStudentList.add( StudentData("문상현", 1997, "부천시 원미구") )
+        mStudentList.add( StudentData("이형종", 1978, "서울시 강서구") )
+        mStudentList.add( StudentData("최다영", 1990, "서울시 서대문구") )
+        mStudentList.add( StudentData("최민서", 1995, "서울시 송파구") )
+        mStudentList.add( StudentData("유석균", 1970, "서울시 동대문구") )
+
+        mAdapter = StudentAdapter( this , R.layout.student_list_item , mStudentList)
+
+        studentListView.adapter = mAdapter
+        
+        studentListView.setOnItemClickListener { parent, view, position, id -> 
+            
+//            position : 몇번줄이 눌렀나? 0번줄 부터 출발
+            
+            val clickedStudent = mStudentList[position]
+
+            Toast.makeText(this, "${clickedStudent.name} 학생이 클릭됨", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/listview_220226/adapters/StudentAdapter.kt
+++ app/src/main/java/com/example/listview_220226/adapters/StudentAdapter.kt
@@ -1,7 +1,12 @@
 package com.example.listview_220226.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.example.listview_220226.R
 import com.example.listview_220226.datas.StudentData
 
 class StudentAdapter (
@@ -10,4 +15,38 @@ class StudentAdapter (
     val mList : ArrayList<StudentData>  // 그래서 실제로 뿌려줄 학생들은 누구누구냐? 목록
 )
     : ArrayAdapter<StudentData>(mContext,resId,mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.student_list_item ,null)
+        }
+
+        val row = tempRow!!
+
+//        각 row의 UI 에 > 자기 자리에 맞는 사람 데이터를 반영.
+
+//        mList 변수에 > 액티비티에 적어넣은 실제 학생 목록이 들어있다.
+
+        val data = mList[ position ]
+
+//        row의 텍스트 뷰 등등에 , data 변수가 가진 하위 정보들을 반영
+
+//        row의 UI태그들을 >> 직접 변수로 만들어내야함.
+        val txtStudentName = row.findViewById<TextView>(R.id.txtStudentName)
+        val txtAge = row.findViewById<TextView>(R.id.txtAge)
+        val txtAddress = row.findViewById<TextView>(R.id.txtAddress)
+
+        txtStudentName.text = data.name
+        txtAddress.text = data.address
+
+        val age = 2022 - data.birthYear + 1
+        txtAge.text = "(${age}세) "
+        
+
+        return row
+    }
 }
