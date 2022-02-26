9ff87b9e3ac36a35bea162baf74d6233df4fe4fb
TFQ_Moon <no1msh1217@gmail.com>
2022-02-26 14:56:18 +0900

    어댑터 클래스 추가 제작 중  - ArrayAdapter를 기본 모양으로 상속 받았더니 에러 발생

---
+++ app/src/main/java/com/example/listview_220226/adapters/StudentAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.listview_220226.adapters
+
+import android.widget.ArrayAdapter
+import com.example.listview_220226.datas.StudentData
+
+class StudentAdapter : ArrayAdapter<StudentData>() {
+}
---
+++ app/src/main/java/com/example/listview_220226/datas/StudentData.kt
@@ -0,0 +1,8 @@
+package com.example.listview_220226.datas
+
+class StudentData(
+    val name: String,
+    val birthYear: Int,
+    val address: String,
+) {
+}
